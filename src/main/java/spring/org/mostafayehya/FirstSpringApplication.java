eb6f1ed773c130dc8c88527b6ca7fc58de95a100
MostafaYehya <mostafa.yehya@cyoda.com>
2022-01-18 15:00:05 +0200

    Add Spring boot support

---
+++ src/main/java/spring/org/mostafayehya/FirstSpringApplication.java
@@ -0,0 +1,12 @@
+package spring.org.mostafayehya;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FirstSpringApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(FirstSpringApplication.class, args);
+    }
+
+}
